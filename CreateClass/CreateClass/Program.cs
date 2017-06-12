455445ca689d2a3ac6a4470ca6435d74e32a1943
vetesipeti <vetesipeti@gmail.com>
2017-06-12 07:09:33 +0200

    Minimal change in the console output.

--- CreateClass/CreateClass/Program.cs
+++ CreateClass/CreateClass/Program.cs
@@ -8,8 +8,8 @@ namespace CreateClass
         {
             Employee Kovacs = new Employee("Géza", DateTime.Now, 1000, "léhűtő");
             Employee Kovacs2 = (Employee)Kovacs.Clone();
-            Console.WriteLine(Kovacs.ToString());
-            Console.WriteLine(Kovacs2.ToString());
+            Console.WriteLine("Kovacs: " + Kovacs.ToString());
+            Console.WriteLine("Kovacs2: " + Kovacs2.ToString());
             Console.ReadKey();
         }
     }
